8b3f09aa36c0aef09cf1d60cc3d224e579e64c84
unknown <antoineb374@gmail.com>
2018-11-25 12:15:01 -0700

    finished add document, some error cheking still needed

--- src/OperatorHelper.java
+++ src/OperatorHelper.java
@@ -2,8 +2,50 @@ import java.util.Scanner;
 
 public class OperatorHelper {
 
+
 	static public void addItem(Scanner scanner) {
 		
+		scanner.nextLine();
+		
+		System.out.println("Please enter the title: ");
+		String title = scanner.nextLine();
+
+		System.out.println("Please enter the author name: ");
+		String aName = scanner.nextLine();
+		
+		System.out.println("Please enter the contents: ");
+		String contents = scanner.nextLine();
+		
+		System.out.println("Please enter the ISBN: ");
+		String ISBN = scanner.nextLine();
+		
+		System.out.println("Please enter the price: ");
+		while(!scanner.hasNextDouble()) {}
+		double price = scanner.nextDouble();
+		
+		System.out.println("Please enter the doucment type: "
+				+ "1: Book\n"
+				+ "2: Magazine\n"
+				+ "3: Journal\n");
+		while(!scanner.hasNextInt()) {}
+		int type = scanner.nextInt();
+		Database d = Database.getInstance();
+		
+		if(type == 1) {
+			System.out.println("Book added to system");	
+			d.addDocument(new Book(title, aName, contents, ISBN, price));
+		}
+		else if(type == 2) {
+			System.out.println("Magazine added to system");	
+			d.addDocument(new Magazine(title, aName, contents, ISBN, price));
+		}
+		else if(type == 3){
+			System.out.println("Journal added to system");	
+			d.addDocument(new Journal(title, aName, contents, ISBN, price));
+		}
+		else {
+			System.out.println("Ivalid menue selection, document not added");
+		}
 	}
 
 	/*
@@ -14,7 +56,18 @@ public class OperatorHelper {
 	 */
 	static public void editItem(Scanner scanner, int choice) {
 		
-		Document d = SearchHelper.searchInventory();
+		switch(choice){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			System.out.println("\nNot a valid input.\n");
+		}
 		
 
 	}
@@ -24,6 +77,8 @@ public class OperatorHelper {
 		Document d = SearchHelper.searchInventory();
 		if(d == null)
 			return;
+		
+		scanner.nextLine();
 		
 		System.out.println("Would you like to delete this document? [Y/N]");
 		System.out.println("Choice:");
@@ -48,5 +103,6 @@ public class OperatorHelper {
 		}
 
 	}
+	
 
 }
